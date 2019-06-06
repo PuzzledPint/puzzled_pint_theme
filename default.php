f63dec9d3af626b282fe9c76a31b11cf51ea523a
Victor Asteinza <vic+github@civrot.org>
2019-06-05 21:28:50 -0700

    Upgrade piwik to matomo

--- default.php
+++ default.php
@@ -65,24 +65,24 @@
 				</div>
 				<?php Loader::element('footer_required'); ?>
 		</div>
-		<!-- Piwik -->
-		<script type="text/javascript">
-		  var _paq = _paq || [];
-		  _paq.push(["trackPageView"]);
-		  _paq.push(["enableLinkTracking"]);
-
-		  (function() {
-		    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.victorasteinza.com/";
-		    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-		    _paq.push(["setSiteId", "3"]);
-		    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-		    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-		  })();
-		</script>
-		<!-- Piwik Image Tracker -->
-		<noscript>
-		  <img src="http://piwik.victorasteinza.com/piwik.php?idsite=3&amp;rec=1" style="border:0" alt="" />
-		</noscript>
-		<!-- End Piwik Code -->
+    <!-- Matomo -->
+    <script type="text/javascript">
+      var _paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+      _paq.push(["setCookieDomain", "*.puzzledpint.com"]);
+      _paq.push(["setDomains", ["*.puzzledpint.com","*.beta.puzzledpint.com","*.puzzlepint.com","*.www.puzzledpint.com","*.www.puzzlepint.com"]]);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//piwik.victorasteinza.com/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '3']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="//piwik.victorasteinza.com/matomo.php?idsite=3&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Matomo Code -->
 	</body>
 </html>
